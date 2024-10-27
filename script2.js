1398b140758f5f614c086932eb9907ec8a89f10a
Jovonijohnson <Jovenom25@yahoo.com>
2024-10-26 20:30:05 -0700

    Edited script2.js with descriptions

--- script2.js
+++ script2.js
@@ -9,3 +9,17 @@
 // 14
 // FizzBuzz
 // ...
+for (let i = 1; i <= 100; i++) { // For loop that begins at 1 and increases by 1 until it reaches 100.
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz'); // Prints "FizzBuzz" to the console if "i" is divisible by both 3 and 5.
+    } else if (i % 3 === 0) {
+
+      console.log('Fizz'); // Outputs "Fizz" to the console when the first condition is false, but the value of "i" is still divisible by 3
+    } else if (i % 5 === 0) {
+
+      console.log('Buzz'); // Prints "Buzz" to the console if "i" is not divisible by both 3 and 5, but is divisible by 5 only.
+    } else {
+
+      console.log(i); // Outputs numerical value of "i" to the console when none of the above conditions are met
+    }
+}
